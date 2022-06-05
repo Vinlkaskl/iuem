a6b0b3d30ad7756453ec5b222d803e31490d0187
Vinlkaskl <106913837+Vinlkaskl@users.noreply.github.com>
2022-06-05 20:24:00 +0700

    message

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! Xin chào bé iu của anh 🖐🖐🖐',
+    introDesc: `Anh có điều muốn nói với bé`,
+    btnIntro: 'OK',
+    title: 'Anh xin lỗi bé iu của anh 😙',
+    desc: 'Em thoát ra, tức là Em không giận Anh nữa đó nha :> ',
+    btnYes: 'Bé yêu anhh 💖💖💖',
+    btnNo: '💔 Bé chả yêu anh ! 💔 ',
+    question:'Tại sao bé lại yêu anh nhỉ?😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh quá là dễ thương lun 😊😊',
+    mess: 'Anh biết mà, anh cũng yêu em lắm í🥰. Moaaa moaaa 😘😘',
+    messDesc: '💬💬 Đừng giận anh nữa nhaaa. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100036140027208'
+}
